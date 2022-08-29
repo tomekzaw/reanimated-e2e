0fb254305c47c19a72fc8df8ba5cf615c4231341
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-08-29 17:24:59 +0200

    Fix TypeScript

--- e2e/tests.e2e.ts
+++ e2e/tests.e2e.ts
@@ -1,13 +1,7 @@
+import {by, element, expect} from 'detox';
+
 describe('Example', () => {
-  beforeAll(async () => {
-    await device.launchApp();
-  });
-
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-
-  it.each([1, 2, 3, 4, 5])('increments couter after tap %d times', async (count) => {
+  it.each([1, 2, 3])('increments couter after tap %d times', async count => {
     await expect(element(by.id('text'))).toHaveText('Count: 0');
     for (let i = 0; i < count; i++) {
       await element(by.id('button')).tap();
