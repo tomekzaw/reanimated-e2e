11ed9b5931f6a6de510f433451a2005afd424f71
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-08-30 10:12:42 +0200

    Add `waitFor`

--- e2e/tests.e2e.ts
+++ e2e/tests.e2e.ts
@@ -2,6 +2,9 @@ import {by, element, expect} from 'detox';
 
 describe('Example', () => {
   it.each([1, 2, 3])('increments couter after tap %d times', async count => {
+    await waitFor(element(by.id('text')))
+      .toBeVisible()
+      .withTimeout(1000);
     await expect(element(by.id('text'))).toHaveText('Count: 0');
     for (let i = 0; i < count; i++) {
       await element(by.id('button')).tap();
