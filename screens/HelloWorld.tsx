3cc3e8f64cac7258b984a0ae23e031d53be043d6
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-09-13 12:12:54 +0200

    Update HelloWorld.tsx

--- screens/HelloWorld.tsx
+++ screens/HelloWorld.tsx
@@ -1,12 +1,32 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
 import React from 'react';
+import {version as reactNativeVersion} from 'react-native/package.json';
+import {version as reanimatedVersion} from 'react-native-reanimated/package.json';
 import {testProps} from '../utils';
+
+function getPlatform() {
+  // @ts-ignore
+  return `${Platform.constants.systemName} ${Platform.Version}`;
+}
+
+function getRuntime() {
+  return 'HermesInternal' in global ? 'Hermes' : 'JSC'; // TODO: V8
+}
+
+function getArchitecture() {
+  return '_nativeFabricUIManager' in global ? 'Fabric' : 'Paper';
+}
 
 export function HelloWorld() {
   return (
     <View style={styles.container}>
       <Text {...testProps('text')}>Hello world!</Text>
+      <Text>React Native version: {reactNativeVersion}</Text>
+      <Text>Reanimated version: {reanimatedVersion}</Text>
+      <Text>Platform: {getPlatform()}</Text>
+      <Text>Architecture: {getArchitecture()}</Text>
+      <Text>Runtime: {getRuntime()}</Text>
     </View>
   );
 }
