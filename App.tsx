866a95bdc9f66d9b809d7220c3bf9411487c6dd7
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-09-19 11:01:09 +0200

    Add other libraries

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import {Button, StyleSheet, View} from 'react-native';
 
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import React from 'react';
 import {TESTS} from './screens/tests';
 import {testProps} from './utils';
@@ -8,27 +9,29 @@ export default function App() {
   const [currentTest, setCurrentTest] = React.useState<() => JSX.Element>();
 
   return (
-    <View style={styles.container}>
-      {currentTest !== undefined ? (
-        <>
-          {React.createElement(currentTest)}
-          <Button
-            title="Menu"
-            {...testProps('menu')}
-            onPress={() => setCurrentTest(undefined)}
-          />
-        </>
-      ) : (
-        TESTS.map(test => (
-          <Button
-            key={test.name}
-            title={test.name}
-            {...testProps(test.name)}
-            onPress={() => setCurrentTest(() => test.component)}
-          />
-        ))
-      )}
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        {currentTest !== undefined ? (
+          <>
+            {React.createElement(currentTest)}
+            <Button
+              title="Menu"
+              {...testProps('menu')}
+              onPress={() => setCurrentTest(undefined)}
+            />
+          </>
+        ) : (
+          TESTS.map(test => (
+            <Button
+              key={test.name}
+              title={test.name}
+              {...testProps(test.name)}
+              onPress={() => setCurrentTest(() => test.component)}
+            />
+          ))
+        )}
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
