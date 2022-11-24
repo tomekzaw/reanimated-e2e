2b5280cb2c076eda78fcadf15bc6a967779a77eb
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-11-24 13:15:55 +0100

    Increase timeouts

--- __tests__/e2e.test.ts
+++ __tests__/e2e.test.ts
@@ -190,15 +190,15 @@ describe('Appium with Jest automation testing', () => {
   if (process.env.VARIANT === 'debug') {
     test('reload once', async () => {
       await openTest('Reload');
-      await client.pause(1000);
+      await client.pause(10_000);
 
       const button = await client.$('~button');
       await button.click(); // reload app
-      await client.pause(10_000);
+      await client.pause(30_000);
       waitForApp();
 
       await openTest('Reload');
-      await client.pause(1000);
+      await client.pause(10_000);
     });
   }
 });
